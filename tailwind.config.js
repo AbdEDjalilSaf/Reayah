bff24a3086f330cfd7c4076d5ac09a7e486699e5
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-06 23:40:23 +0100

    adding variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                green: "#32DEBF",
+                green_b: "#0D8872",
+                perpol: "#9747FF",
+                perpol_b: "#7C2CBF",
+                black_text: "#1f1f1f",
+                gray: "#525252",
+                gray_white: "#e6e6e6",
+                
+            },
+        },
     },
     plugins: [],
 };
