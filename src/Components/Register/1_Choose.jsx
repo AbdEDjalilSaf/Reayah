db6e13b176b700adcd57e701ba599e45db449e3d
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-12 15:32:20 +0100

    finishing with first Step

--- src/Components/Register/1_Choose.jsx
+++ src/Components/Register/1_Choose.jsx
@@ -1,16 +1,76 @@
-import React from 'react'
+import React from "react";
 import ImageLoader from "../ImageLoader";
 import Doctore from "../../../public/Register/Doctore.png";
 import Patient from "../../../public/Register/Patient.png";
-function Choose() {
-  return (
-      <div>
-          <div className=" relative">
-              <div>SignUp</div>
-              <span className=" hidden md:block absolute  left-0 -z-10 bottom-[2px] w-full h-2 rounded-xl bg-green "></span>
-          </div>
-      </div>
-  );
+import { Link } from "react-router-dom";
+function Choose({ user, change_user, setUserType_Done }) {
+    return (
+        <div className=" pt-12">
+            <div className=" text-3xl font-semibold text-perpol text-center ">
+                <div className=" relative w-fit m-auto">
+                    SignUp
+                    <span className=" hidden md:block absolute  left-0 -z-10 bottom-[2px] w-full h-2 rounded-xl bg-green "></span>
+                </div>
+            </div>
+            <div className=" font-semibold text-gray text-2xl text-center pt-6 ">
+                as a patient or doctor
+            </div>
+            {/* Cards */}
+            <div className=" flex items-center justify-center gap-8 pt-6">
+                <div
+                    className={` bg-perpol bg-opacity-20 text-perpol text-2xl  font-semibold ${
+                        user.Type == "Patient" && "border-4 border-perpol"
+                    } p-4 w-[360px] h-[245px] flex flex-col  justify-between rounded-xl  cursor-pointer `}
+                    onClick={() => change_user("Type", "Patient")}
+                >
+                    <div>I’m a patient , need a treatments </div>
+                    <div className=" flex justify-between items-end">
+                        <div className=" w-6 h-6 rounded-full border-2 border-perpol  flex items-center  justify-center relative   ">
+                            {user.Type === "Patient" && (
+                                <div className="w-4 h-4 rounded-full bg-perpol m-auto absolute"></div>
+                            )}
+                        </div>
+                        <div className="relative w-20 h-20 ">
+                            <ImageLoader src={Patient} />
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    className={` bg-green bg-opacity-10 text-green text-2xl font-semibold ${
+                        user.Type == "Doctor" && "border-4 border-green"
+                    }  p-4  w-[360px] h-[245px] flex flex-col  justify-between rounded-xl  cursor-pointer `}
+                    onClick={() => change_user("Type", "Doctor")}
+                >
+                    <div>I’m a doctor</div>
+                    <div className=" flex justify-between items-end">
+                        <div className=" w-6 h-6 rounded-full border-2 border-green flex items-center  justify-center relative">
+                            {user.Type === "Doctor" && (
+                                <div className="w-4 h-4 rounded-full bg-green m-auto absolute"></div>
+                            )}
+                        </div>
+
+                        <div className="relative w-20 h-20 ">
+                            <ImageLoader src={Doctore} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className=" bg-perpol text-white font-semibold text-xl p-3 cursor-pointer rounded-xl w-fit m-auto mt-6 text-center"
+                onClick={setUserType_Done(true)}
+            >
+                Create Account
+            </div>
+            <div className=" text-xl text-gray font-semibold text-center pt-6">
+                Already have an account?{" "}
+                <Link className="text-green " to={"/Login"}>
+                    Log In
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default Choose
+export default Choose;
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import Choose from "./1_Choose";
 function Register() {
-    const [step, setStep] = useState(1);
-    const [user_Type, setUser_Type] = useState()
+    // const [step, setStep] = useState(1);
     
+    const [user, setUser] = useState({
+        Type: "Patient",
+        Fitst_Name: "",
+        Last_Name: "",
+        Phone_Number: "",
+        Email: "",
+        Password: "",
+    });
+    const change_user =(key, value)=>{
+        setUser({
+            ...user,
+            [key]: value
+        })
+    }
+    const [userType_Done, setUserType_Done] = useState(false)
+    const [EmailVerification_Done, setEmailVerification_Done] = useState(false)
+    const [Form_Done, setForm_Done] = useState(false)
+
     return (
-        <div></div>
-            
-        
+        <div>
+            <div>
+                <Choose
+                    setUserType_Done={setUserType_Done}
+                    user={user}
+                    change_user={change_user}
+                />
+            </div>
+        </div>
     );
 }
 
-export default Register
+export default Register;
