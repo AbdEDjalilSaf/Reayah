b95201d21bd267529fd93d11b958b803055850c7
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-06-02 17:46:16 +0100

    Update npm dependencies and project structure

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import WhyChooseReaya from "./WhyChooseReaya/WhyChooseReaya";
+import AdsAfterChoose from "./AdsAfterChoose/AdsAfterChoose";
+import Team from "./Team/Team";
+import AboutHero from "./AboutHero/AboutHero";
+import KeyFeatures from "./KeyFeatures/KeyFeatures";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div>
+      <AboutHero />
+      <WhyChooseReaya />
+      <AdsAfterChoose />
+      <Team />
+      <KeyFeatures />
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- src/Router.jsx
+++ src/Router.jsx
@@ -5,58 +5,61 @@ import Not_Finished from "./Components/Not_Finished";
 import Not_Found from "./Components/Not_Found";
 import Register from "./Components/Auth/Register/Register";
 import Login from "./Components/Auth/Login/Login";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import FAQ from "./Components/FAQ/FAQ";
+import ContactUs from "./Components/ContactUs/ContactUs";
 const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                // path: "/",
-                index: true,
-                element: <Home />,
-            },
-            {
-                path: "/Blogs",
-                element: <Not_Finished />,
-            },
-            {
-                path: "/Contact",
-                element: <Not_Finished />,
-            },
-            {
-                path: "/About",
-                element: <Not_Finished />,
-            },
-            {
-                path: "/FAQ",
-                element: <Not_Finished />,
-            },
-            {
-                path: "*",
-                element: <Not_Found />,
-            },
-            {
-                path: "/Register",
-                element: <Register />,
-            },
-            {
-                path: "/Login",
-                element: <Login />,
-            },
-        ],
-    },
-
-    {
-        path: "/Profile/:id",
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        // path: "/",
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/Blogs",
         element: <Not_Finished />,
-    },
-    {
-        path: "/Dashboard/:id",
-        element: <Not_Finished />,
-    },
-    {
+      },
+      {
+        path: "/Contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "/About",
+        element: <AboutUs />,
+      },
+      {
+        path: "/FAQ",
+        element: <FAQ />,
+      },
+      {
         path: "*",
         element: <Not_Found />,
-    },
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+    ],
+  },
+
+  {
+    path: "/Profile/:id",
+    element: <Not_Finished />,
+  },
+  {
+    path: "/Dashboard/:id",
+    element: <Not_Finished />,
+  },
+  {
+    path: "*",
+    element: <Not_Found />,
+  },
 ]);
 export default routes;
