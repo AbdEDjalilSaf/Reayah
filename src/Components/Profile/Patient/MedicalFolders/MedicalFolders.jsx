c322a2110dda028ae9c8ceae45048cabe7ca3b29
imadbenmadi <imadbenmadi891@gmail.com>
2024-06-08 21:27:54 +0100

    init

--- src/Components/Profile/Patient/MedicalFolders/MedicalFolders.jsx
+++ src/Components/Profile/Patient/MedicalFolders/MedicalFolders.jsx
@@ -45,10 +45,14 @@ function Folders() {
     useEffect(() => {
         fetchData();
     }, []);
+    const [addFolder, setaddFolder] = useState(false);
+    function toogleAddFolder() {
+        setaddFolder(!addFolder);
+    }
     if (loading) {
         return (
             <div
-                className=" w-screen h-screen flex items-center 
+                className=" w-[80vw] h-screen flex items-center 
             justify-center gap-5 flex-col"
             >
                 <img src={Logo_perpole} className=" w-24" alt="" />
@@ -57,21 +61,51 @@ function Folders() {
         );
     }
     return (
-        <div className=" bg-perpol bg-opacity-40 flex items-center justify-center w-full min-h-screen">
-            <div className=" w-[96%] min-h-[95vh] mx-auto bg-white rounded-lg  ">
-                <div className=" flex justify-between mx-10">
+        <div className=" bg-perpol bg-opacity-40 flex items-center justify-center w-full min-h-screen ">
+            <div className=" w-[96%] min-h-[95vh] mx-auto bg-white rounded-lg  relative">
+                <div className=" flex justify-between mx-10  h-full">
                     <div className=" text-3xl pt-6 pl-6 text-perpol font-bold ">
                         Folders
                     </div>
-                    <Link
-                        to={`/Patients/${patientId}/Medical_Folders/Add`}
-                        className=" flex items-center justify-center pt-6 gap-2"
+
+                    <div
+                        onClick={toogleAddFolder}
+                        className=" flex items-center justify-center pt-6 gap-2 cursor-pointer"
                     >
                         <HiOutlineFolderAdd className=" text-3xl text-perpol" />
                         <div className=" text-2xl text-perpol">Add Folder</div>
-                    </Link>
+                    </div>
                 </div>
-
+                {addFolder && (
+                    <div
+                        className="absolute top-0 left-50 -translate-x-50 -translate-y-50 w-full h-full mx-auto rounded-lg
+                     bg-opacity-50 flex items-center justify-center  transition-all duration-300 bg-gray "
+                    >
+                        <div className=" w-[50%] rounded-lg p-6 bg-white">
+                            <div className=" text-3xl text-perpol font-bold text-center">
+                                Add Folder
+                            </div>
+                            <div className=" text-2xl text-gray-600 font-semibold">
+                                Name
+                            </div>
+                            <input
+                                type="text"
+                                className=" w-full h-12 border-2 border-gray-200 rounded-lg px-4"
+                            />
+                            <div className=" flex items-center justify-between">
+                                <button className=" w-1/2 h-12 bg-perpol text-white rounded-lg mt-4">
+                                    Add
+                                </button>
+                                <button
+                                    onClick={toogleAddFolder}
+                                    className=" w-1/2 h-12 bg-gray-200 text-gray-600 rounded-lg mt-4"
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
                 <div className=" pt-12">
                     {Folders.length == 0 ? (
                         <div className=" flex flex-col gap-4 ">
