18c31ff5b3373bef08473b7cf09913eaed09f40b
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-06-08 22:23:51 +0100

    Refactor Patients component styling for Doctore profile

--- src/Components/Profile/Doctore/Patients.jsx
+++ src/Components/Profile/Doctore/Patients.jsx
@@ -3,7 +3,7 @@ import PatientsCard from "./Patients/PatientsCard";
 
 function Patients() {
   return (
-    <div className="w-full  min-h-screen h-fit  bg-purple-100 rounded-2xl flex-col flex justify-center items-center ">
+    <div className="w-full  min-h-screen h-fit  bg-purple-100 rounded-2xl flex-col flex justify-start items-center ">
       <div className="w-[90%] my-6 mx-auto h-44 px-16 py-14 bg-gradient-to-r from-purple-700 to-purple-400 rounded-2xl justify-center items-center gap-96 inline-flex">
         <div className="text-white text-5xl font-semibold font-['Outfit']">
           Patients
